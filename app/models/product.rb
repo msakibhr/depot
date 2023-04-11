47e362a20fb9ad591f8dd5e8dd04781632df7e07
msakibhr <msakibhr@gmail.com>
2023-04-11 15:04:47 +0600

    Iteration B1 slighly edited

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,7 +11,7 @@ class Product < ApplicationRecord
   #
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(gif|jpg|png)\z}i,
+    with:    %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
